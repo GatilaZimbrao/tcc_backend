51967e02542c42915ae628411f7947beae362c18
Gabriel Átila <gabriel.atila.zimbrao@gmail.com>
2024-04-26 10:20:06 -0300

    fix(file): fix in aloowed ext file

--- src/modules/file/config/config.ts
+++ src/modules/file/config/config.ts
@@ -1,4 +1,14 @@
-export const FILE_ALLOWED_EXT: String[] = [".pdf", ".png", ".jpg", ".jpeg"];
+export const FILE_ALLOWED_EXT: String[] = [
+  ".txt",
+  ".pdf",
+  ".odt",
+  ".doc",
+  ".docx",
+  ".png",
+  ".jpg",
+  ".jpeg",
+  ".webp",
+];
 
 export const FILE_SIZE_LIMIT_MB = 5; // 5 MB
 export const FILE_SIZE_LIMIT = FILE_SIZE_LIMIT_MB * 1024 * 1024;
